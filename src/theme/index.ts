3dc557b7a5069469d0e0a520a2f58ef93ca70509
BinhTX <binh.xuantran@digicommercegroup.com>
2025-10-29 15:16:38 +0700

    remove code unused

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,7 +1,4 @@
 export { colors } from './colors';
 export type { Colors } from './colors';
 
-export { typography } from './typography';
-export type { Typography } from './typography';
-
 export * from './useTheme';
